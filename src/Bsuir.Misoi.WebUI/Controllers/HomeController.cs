55c1534f8df0822675aefa0ef55be06950a3cbe9
Vadzim Vysotski <vysotski.vadim@gmail.com>
2015-09-28 00:05:31 +0300

    add shitty home controller

---
+++ src/Bsuir.Misoi.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Http;
+using System.IO;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Bsuir.Misoi.WebUI.Controllers
+{
+	public class HomeController : Controller
+	{
+		[Route("")]
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[Route("")]
+		[HttpPost]
+		public IActionResult Index(IFormFile file)
+		{
+			using (MemoryStream ms = new MemoryStream())
+			{
+				file.OpenReadStream().CopyTo(ms);
+				return this.File(ms.ToArray(), file.ContentType);
+			}
+		}
+	}
+}
