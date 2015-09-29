b4032129826abf5968eabe0ee2398154706e8556
Vadzim Vysotski <vysotski.vadim@gmail.com>
2015-09-29 19:31:01 +0300

    fix perfomance issue

--- src/Bsuir.Misoi.Core/Images/Implementation/BitmapImage.cs
+++ src/Bsuir.Misoi.Core/Images/Implementation/BitmapImage.cs
@@ -2,32 +2,54 @@
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
+using System.Runtime.InteropServices;
 
 namespace Bsuir.Misoi.Core.Images.Implementation
 {
 	public class BitmapImage : IImage
 	{
-		private readonly Bitmap _bitmap;
+		private byte[] _pixelBuffer;
+		private int _bitMapStride;
+		private int _height;
+		private int _width;
 
 		public BitmapImage(Bitmap bitmap)
 		{
-			_bitmap = bitmap;
+			_height = bitmap.Height;
+			_width = bitmap.Width;
+			BitmapData bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+													ImageLockMode.ReadOnly,
+												    PixelFormat.Format32bppArgb);
+			_bitMapStride = bitmapData.Stride;
+            _pixelBuffer = new byte[_bitMapStride * bitmap.Height];
+			Marshal.Copy(bitmapData.Scan0, _pixelBuffer, 0, _pixelBuffer.Length);
+			bitmap.UnlockBits(bitmapData);
 		}
 
-		public int Height => _bitmap.Height;
+		public int Height => _height;
 
 		public string Name { get; set; }
 
-		public int Width => _bitmap.Width;
+		public int Width => _width;
 
 		public Pixel GetPixel(int x, int y)
 		{
-			var color = _bitmap.GetPixel(x, y);
-			return new Pixel(color);
+			int byteOffset = y * _bitMapStride + x * 4;
+			return new Pixel { B = _pixelBuffer[byteOffset], G = _pixelBuffer[byteOffset + 1], R = _pixelBuffer[byteOffset + 2] };
 		}
 
 		public void Save(Stream saveStream)
 		{
+			Bitmap resultBitmap = new Bitmap(_width, _height);
+
+			BitmapData resultData = resultBitmap.LockBits(new Rectangle(0, 0,
+									 resultBitmap.Width, resultBitmap.Height),
+													  ImageLockMode.WriteOnly,
+												 PixelFormat.Format32bppArgb);
+
+			Marshal.Copy(_pixelBuffer, 0, resultData.Scan0, _pixelBuffer.Length);
+			resultBitmap.UnlockBits(resultData);
+
 			var extension = Path.GetExtension(this.Name);
 			ImageFormat imageFormat;
 			if (extension == "png")
@@ -38,13 +60,15 @@ namespace Bsuir.Misoi.Core.Images.Implementation
 			{
 				imageFormat = ImageFormat.Jpeg;
 			}
-            _bitmap.Save(saveStream, imageFormat);
+            resultBitmap.Save(saveStream, imageFormat);
 		}
 
 		public void SetPixel(int x, int y, Pixel pixel)
 		{
-			var color = Color.FromArgb(0, pixel.R, pixel.G, pixel.B);
-			_bitmap.SetPixel(x, y, color);
+			int byteOffset = y * _bitMapStride + x * 4;
+			_pixelBuffer[byteOffset] = pixel.B;
+			_pixelBuffer[byteOffset + 1] = pixel.G;
+			_pixelBuffer[byteOffset + 2] = pixel.R;
 		}
 	}
 }
