bfe76999b78e88c3bd428fc04797eeb0414d5fab
Miha_Strunin <321>
2015-10-28 00:44:45 +0300

    GOD COMMIT

--- src/Bsuir.Misoi.Core/Images/Finding/Implementation/Segmentation.cs
+++ src/Bsuir.Misoi.Core/Images/Finding/Implementation/Segmentation.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using Bsuir.Misoi.Core.Images.Filtering;
 using Bsuir.Misoi.Core.Images.Filtering.Implementation;
+using System;
 
 namespace Bsuir.Misoi.Core.Images.Finding.Implementation
 {
@@ -12,7 +13,7 @@ namespace Bsuir.Misoi.Core.Images.Finding.Implementation
 
         // данные и 3 метода  для работы с системой непересекающихся множеств
         private int[] segment = new int[100000]; // массив эквивалентых класов-сегментов
-        private  int[] rank = new int[100000]; // ранги эквивалентных классов, в общем-то тут не обязательны
+        private int[] rank = new int[100000]; // ранги эквивалентных классов, в общем-то тут не обязательны
 
         public Segmentation(IFilter binarizationFilter)
         {
@@ -48,21 +49,21 @@ namespace Bsuir.Misoi.Core.Images.Finding.Implementation
         }
 
         private int BinaryPixel(Pixel pixel)  // перевод RGB пикселя в строго бинарный вид типа int 
-        { 
-            if (pixel.B != 0) { return 1;}   // или R!=0 или G != 0  ?)) 
+        {
+            if (pixel.B != 0) { return 1; }   // или R!=0 или G != 0  ?)) 
             else { return 0; }
         }
 
         private int[,] image;  //  image in int-matrix type
 
-        
+
 
         private void ConvertImageToBinaryMatrix(IImage inputImage)  // перевод пиксельного изображения в матричный вид типа int
-        { 
+        {
             image = new int[inputImage.Width, inputImage.Height];
-            for (int i =0; i < inputImage.Width; i++  )
+            for (int j = 0; j < inputImage.Height; j++)
             {
-                for (int j = 0; j < inputImage.Height; j++)
+                for (int i = 0; i < inputImage.Width; i++)
                 {
                     image[i, j] = BinaryPixel(inputImage.GetPixel(i, j));
                 }
@@ -70,39 +71,39 @@ namespace Bsuir.Misoi.Core.Images.Finding.Implementation
         }
 
         public IEnumerable<IFindResult> Find(IImage inputImage)   // конкретно сегментация  работает c бинарной матрицой - быстрее будет, чем работа с пиксельным представлением изображения
-        {           
+        {
             _binarizationFilter.Filter(inputImage);
             ConvertImageToBinaryMatrix(inputImage);
 
             int segmentNum = 1; // номер нового класса, бинарное изображение или 0 или 1, считанём с 2ух :D
             int B = 0, C = 0, A = 0;
-           
-            for (int i = 0; i < inputImage.Width; i++)  // Цикл по пикселям изображения
+
+            for (int j = 0; j < inputImage.Height; j++)  // Цикл по пикселям изображения
             {
-                for (int j = 0; j < inputImage.Height; j++)
+                for (int i = 0; i < inputImage.Width; i++)
                 {
-                    if (j - 1 < 0)
+                    if (i - 1 < 0)
                     {
                         B = 0;
                     }
                     else
                     {
-                        B = image[i, j - 1];
+                        B = image[i - 1, j];
                     }
 
-                    if (i - 1 < 0)
+                    if (j - 1 < 0)
                     {
                         C = 0;
                     }
                     else
                     {
-                        C = image[i - 1, j];
+                        C = image[i, j - 1];
                     }
 
                     A = image[i, j];
                     if (A == 0) { } // Если в текущем пикселе пусто - то ничего не делаем
                     else if (B == 0 & C == 0) // Если вокруг нашего пикселя пусто а он не пуст - то это повод подумать о новом сегменте
-                    { 
+                    {
                         image[i, j] = ++segmentNum;  // пометим пикселеь номером нового сегмента 
                         MakeSet(segmentNum);  // выделим новый сегмент, запомним его
                     }
@@ -129,30 +130,58 @@ namespace Bsuir.Misoi.Core.Images.Finding.Implementation
 
                 }// for j
             } // for i
-            
 
 
-            /// ВТОРОЙ ПРОХОД, КОРРЕКТИРОВКА:
 
-            for (int i = 0; i < inputImage.Width; i++)  // Цикл по пикселям изображения
+            // ВТОРОЙ ПРОХОД, КОРРЕКТИРОВКА:
+            int[] squares = new int[20];  
+            int[] perimeters = new int[20];
+            int L = 0, R = 0, U = 0, D = 0;
+
+            for (int j = 0; j < inputImage.Height; j++)  // цикл по пикселям изображения
             {
-                for (int j = 0; j < inputImage.Height; j++)
+                for (int i = 0; i < inputImage.Width; i++)
                 {
                     if (image[i, j] != 0)
                     {
                         image[i, j] = Find(image[i, j]);
+
+                        squares[image[i, j]]++;  // инкремент площади сегмента 
+                                              // check pixel from
+                        U = (j - 1 < 0) ? 1 : image[i, j - 1];   // up
+                        L = (i - 1 < 0) ? 1 : image[i - 1, j];   // left
+                        R = (i + 1 >= 8) ? 1 : image[i + 1, j];  // right
+                        D = (j + 1 >= 8) ? 1 : image[i, j + 1];  // down
+
+                        if (L == 0 || R == 0 || U == 0 || R == 0)
+                        {
+                            perimeters[image[i, j]]++;  // если в стороне - фон - то пиксель входит в приметр
+                        }
                     }
                 }
             }
-            //ВЫВОД МАТРИЦЫ, построенной на изображении
-            //for (int i = 0; i < inputImage.Width; i++)  // Цикл по пикселям изображения
-            //{
-            //    for (int j = 0; j < inputImage.Height; j++)
-            //    {
-            //        Debug.Write($"{image[i, j]} ");
-            //    }
-            //    Debug.WriteLine("");
-            //}
+
+            // вычисляем форм-фактор для фигуры
+            double formFactor = 0;
+            bool[] results = new bool[100000];
+            double[] intResults = new double[100000];
+            for (int i = 0; i < segment.Length; i++)
+            {
+                if (segment[i] != 0)
+                {
+                    formFactor = perimeters[i] / (2 * Math.Sqrt(Math.PI * squares[i]));
+                    if ((formFactor > 0.45) && (formFactor < 0.5)) // 520mm X 113mm  form-factor 0,474
+                    {
+                        results[i] = true;
+                    }
+                    intResults[i] = formFactor; // тестовый - чтобы посмотреть чё получилось по сегментам
+                }
+            }
+
+
+            // ТУТ НУЖНО НАЙТИ ДАННЫЕ
+            //for (int i = 0, )
+            //if (results[i])
 
             yield return new FindResult(10, 10, 20, 20);
         }
