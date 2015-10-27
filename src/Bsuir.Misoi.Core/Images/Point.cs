c54aed94fead8cc5474567cb841ada05f80cce2e
Vadzim Vysotski <vysotski.vadim@gmail.com>
2015-10-27 23:46:58 +0300

    draw contur by line

---
+++ src/Bsuir.Misoi.Core/Images/Point.cs
@@ -0,0 +1,15 @@
+﻿namespace Bsuir.Misoi.Core.Images
+{
+    public struct Point
+    {
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; }
+
+        public int Y { get; }
+    }
+}
